4c48f1579184107c4e0d07607259f4b59eb72d6f
Wazqaz <etienne.deshaies-samson@usherbrooke.ca>
2020-01-12 16:23:43 -0500

    Fixed 2/3 and started ajusting TestBenches

--- sources_1/new/Fonction_2_3.vhd
+++ sources_1/new/Fonction_2_3.vhd
@@ -46,23 +46,40 @@ architecture Behavioral of Fonction_2_3 is
            Cout : inout STD_LOGIC);
     end component;
     
-    signal A : STD_LOGIC_VECTOR (3 downto 0);
-    signal B : STD_LOGIC_VECTOR (3 downto 0);
-    signal Sum : STD_LOGIC_VECTOR (3 downto 0);
+    
+    signal A : STD_LOGIC_VECTOR (7 downto 0);
+    signal B : STD_LOGIC_VECTOR (7 downto 0);
+    
+    signal Sum : STD_LOGIC_VECTOR (7 downto 0);
+    signal SumPlus1 : STD_LOGIC_VECTOR (3 downto 0);
+    
+    signal Cin : STD_LOGIC;
     signal Cout: STD_LOGIC;
+    
 
 begin
     
-    inst_Adder : Add4bitsB
-        port map ( A => A, B => B, Cin => '0', Sum => Sum, Cout => Cout);
+    unit_Adder : Add4bitsB
+        port map ( A => A(7 downto 4), B => B(7 downto 4), Cin => Cin, Sum => Sum(7 downto 4), Cout => Cout);
+        
+    decimal_Adder : Add4bitsB
+        port map ( A => A(3 downto 0), B => B(3 downto 0), Cin => '0', Sum => Sum(3 downto 0), Cout => Cin);
+        
+    add1 : Add4bitsB
+        port map ( A => Sum(7 downto 4), B => "0001", Cin => '0', Sum => SumPlus1, Cout => Cout);
         
     ADCBin_Recieve : Process ( ADCBin )
     begin
-        A <= "000" & ADCBin(3);
-        B <= '0' & ADCBin(3 downto 1);
+    
+        A <= "000" & ADCBin(3 downto 0) & '0';
+        B <= '0' & ADCBin(3 downto 0) & "000";
         
-        -- Add done
-        A2_3 <= Sum(2 downto 0);
+        -- Add done, select if we need to add +1 in edge cases
+        
+        
     end process;
+    
+    A2_3 <= SumPlus1(2 downto 0) when Sum(3) = '1'
+    else Sum(6 downto 4);
     
 end Behavioral;
